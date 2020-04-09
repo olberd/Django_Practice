f70766a5255d090887a22a8e62b1fe02d141f268
Oleg <olberd@bk.ru>
2020-04-09 23:50:02 +0300

    Add Pagination

--- bboard/views.py
+++ bboard/views.py
@@ -6,12 +6,20 @@ from .forms import BbForm
 from .models import Bb, Rubric
 from django.urls import reverse_lazy
 import datetime
+from django.core.paginator import Paginator
 
 
 def index(request):
     bbs = Bb.objects.all()
     rubrics = Rubric.objects.all()
-    context = {'bbs': bbs, 'rubrics': rubrics}
+    paginator = Paginator(bbs, 3)
+    if 'page' in request.GET:
+        page_num = request.GET['page']
+    else:
+        page_num = 1
+    page = paginator.get_page(page_num)
+
+    context = {'rubrics': rubrics, 'page': page, 'bbs': page.object_list}
     return render(request, 'bboard/index.html', context)
 
     # date = datetime.date.today().strftime('%A %d. %B %Y')
@@ -24,7 +32,7 @@ def index(request):
 def by_rubric(request, rubric_id):
     bbs = Bb.objects.filter(rubric=rubric_id)
     rubrics = Rubric.objects.all()
-    current_rubric = Rubric.objects.get(pk = rubric_id)
+    current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
 
@@ -38,6 +46,10 @@ class BbCreateView(CreateView):
         context = super().get_context_data(**kwargs)
         context['rubrics'] = Rubric.objects.all()
         return context
+
+class RubricCreate(CreateView):
+    model = Rubric
+    fields = ['title']
 
 
 
